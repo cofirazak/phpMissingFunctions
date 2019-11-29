5e37be74f6a5c02a3c4c58e5ab994b87f2bb66cd
cofirazak <cofirazak@gmail.com>
2019-11-30 00:12:44 +0300

    Added missingFunctions\StringFunc::mbUcFirst. Php's ucfirst() function with multibyte support.

---
+++ src/StringFunc.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace missingFunctions;
+
+/**
+ * Useful string functions missing in php.
+ */
+class StringFunc
+{
+    /**
+     * Replicates php's ucfirst() function with multibyte support.
+     *
+     * @param string      $str      the string being converted
+     * @param null|string $encoding Optional encoding parameter is the character encoding.
+     *                              If it is omitted, the internal character encoding value will be used.
+     *
+     * @return string the input string with first character uppercased
+     */
+    public function mbUcFirst(string $str, string $encoding = null): string
+    {
+        if (is_null($encoding)) {
+            $encoding = mb_internal_encoding();
+        }
+
+        return mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding) .
+            mb_substr($str, 1, null, $encoding);
+    }
+}
