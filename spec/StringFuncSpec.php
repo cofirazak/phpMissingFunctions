fe71976390506a6229bb049d5991549c07015613
cofirazak <cofirazak@gmail.com>
2019-12-08 00:37:45 +0300

    added it_is_initializable to StringFuncSpec.php, added phpspec.yml

--- spec/StringFuncSpec.php
+++ spec/StringFuncSpec.php
@@ -6,6 +6,11 @@ use PhpSpec\ObjectBehavior;
 
 class StringFuncSpec extends ObjectBehavior
 {
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(StringFunc::class);
+    }
+
     public function it_upper_first_char_of_mb_str()
     {
         $this::mbUcFirst('проверка апперкейса мультибайт строки.')
