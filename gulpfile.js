cf32020d5c25b4abb067ac8b1ac180dc42e01519
cofirazak <cofirazak@gmail.com>
2019-12-08 00:27:08 +0300

    removed big comment from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,3 @@
-/**
- * If you don't like the verbosity level of notifications you can remove the [, verbose: 'v'] at the end of line 35.
- *
- * And if you have more than 1 file in spec directory is better to create phpspec.yml in root project directory,
- * it doesn't matter if phpspec.yml will be empty or properly configured,
- * and comment line 34 and uncomment line 33.
- *
- * For this file to work properly you need to install:
- * brew install node
- * npm install --global gulp-cli
- * npm install gulp gulp-phpspec gulp-notify
- *
- * Just in case my setup:
- * OS: macOS Catalina
- * node -v: v13.2.0
- * npm -v: 6.13.1,
- * npx -v: 6.13.1
- * gulp -v: CLI version: 2.2.0 Local version: 4.0.2
- *
- * package.json:
- * "devDependencies": {
- *   "gulp": "^4.0.2",
- *   "gulp-notify": "^3.2.0",
- *   "gulp-phpspec": "^0.5.5",
- * }
- */
-
 const gulp = require('gulp');
 const phpspec = require('gulp-phpspec');
 const notify = require('gulp-notify');
